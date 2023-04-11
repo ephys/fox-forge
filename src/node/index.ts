c25333ab4a22fcf8b18548a1c0eb0ca43a858a54
Zoé Cox <zoe@ephys.dev>
2023-04-11 16:07:37 +0200

    feat: add fileUrlToDirname

--- src/node/index.ts
+++ src/node/index.ts
@@ -1,4 +1,5 @@
 /** Generated File, do not modify directly. Run "yarn sync" instead */
 
+export * from './file-url-to-dirname.js';
 export * from './is-node-error.js';
 export * from './list-files-recursive.js';
